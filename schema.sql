c0929d3223d8016a5a954632ceae71b22c55dec7
CharlesPBiggar <charles.p.biggar@gmail.com>
2019-10-07 21:23:38 -0400

    removed (,) in foreign key statment

--- schema.sql
+++ schema.sql
@@ -22,7 +22,7 @@ CREATE TABLE dept_emp (
     dept_no VARCHAR(4),
     from_date DATE,
     to_date DATE,
-    FOREIGN KEY (emp_no,) REFERENCES employees(emp_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
