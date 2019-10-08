e377cfda289728595cea577022939938a8d9f241
CharlesPBiggar <charles.p.biggar@gmail.com>
2019-10-07 21:47:34 -0400

    added queries document with employee,salaries join query

---
+++ queries.sql
@@ -0,0 +1,4 @@
+-- List the following details of each employee: employee number, last name, first name, gender, and salary.
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.gender, salaries.salary
+FROM employees
+JOIN salaries ON (salaries.emp_no = employees.emp_no);
